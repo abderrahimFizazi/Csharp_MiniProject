af16752227a05ac0b2ef42c118580f8e238fd4bf
abderrahimFizazi <abderrahim.fazazi@etu.uae.ac.ma>
2023-02-24 11:42:04 +0100

    'first'

---
+++ DatabaseCnx.cs
@@ -0,0 +1,37 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace Prepa
+{
+    internal class DatabaseCnx
+    {
+        static MySqlConnection cnn = null;
+        static string server = "localhost";
+        static string database = "last";
+        static string uid = "root";
+        static string pwd = "";
+        static public MySqlConnection Conexion()
+        {
+            string connetionString = "server="+server+";database="+database+";uid="+uid+";pwd="+pwd+";";
+            cnn = new MySqlConnection(connetionString);
+            try
+            {
+                cnn.Open();
+                Console.WriteLine("Connection Open ! ");
+                return cnn;
+            }catch(MySqlException ex)
+            {
+                Console.WriteLine("Problem with database Connection");
+            }
+            return null;
+        }
+        static public MySqlConnection getConexion()
+        {
+            if(cnn == null)
+            {
+                cnn = Conexion();
+            }
+            return cnn;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using MySql.Data.MySqlClient;
+namespace Prepa
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            GestionEleve C1 = new GestionEleve();
+            Eleve e = new Eleve("abdo","Fizazi", "1000", "3","GINF");
+            Eleve e2 = new Eleve("aya ", "Fizazi", "1001", "3", "GINF");
+            Eleve e3 = new Eleve("safae", "Fizazi", "1002", "3", "GINF");
+            Eleve e4 = new Eleve("simo", "Fizazi", "1004", "3", "GINF");
+            Eleve e5 = new Eleve("yassine", "Fizazi", "1005", "3", "GINF");
+            Eleve leveNexistePas = new Eleve("prof", "ghailani", "1007", "3", "GINF");
+            Eleve updatedUserAbdo = new Eleve("abderrahim", "new Fizazi", "1000", "4", "GSTR");
+
+
+            C1.Update(updatedUserAbdo);
+        }
+    }
+}
